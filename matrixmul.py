8a655478d3b467b3aab5d3c3852f55fce5a981b8
ruabuliuqiu <715762243@qq.com>
2020-02-19 14:34:53 +0800

    matrix_multiplication

---
+++ matrixmul.py
@@ -0,0 +1,24 @@
+#ju zheng xiang cheng
+#liufang 20200219
+n = int(input("Enter the value of n: "))
+print("Enter values for the Matrix A")
+a = []
+for i in range(n):
+    a.append([int(x) for x in input().split()])
+print("Enter values for the Matrix B")
+b = []
+for i in range(n):
+    b.append([int(x) for x in input().split()])
+
+c = []
+for i in range(n):
+    c.append([a[i][j] * b[i][j] for j in range(n)])
+print("After matrix multiplication")
+print("_" * 7 * n)
+for x in c:
+    for y in x:
+        print(str(y).rjust(5), end=' ')
+    print()
+print("_" * 7 * n)
+
+
